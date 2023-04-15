3cd08ddf2f7e05f6f464138a0ee67924af22b9c8
Mattymattk14 <114107459+Mattymattk14@users.noreply.github.com>
2023-04-15 15:44:01 -0600

    rebuild part 1
    get first page of ships + css

--- starwars-api-starships/src/App.js
+++ starwars-api-starships/src/App.js
@@ -1,25 +1,86 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState, useEffect } from "react";
+import axios from "axios";
+import "./App.css";
 
-function App() {
+const Starships = () => {
+  const [starships, setStarships] = useState([]);
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState(null);
+  const [pilots, setPilots] = useState([]);
+
+  const getAllStarships = async (url) => {
+    setLoading(true);
+
+    try {
+      const response = await axios.get(url);
+      const newStarships = [...starships, ...response.data.results];
+
+      if (response.data.next) {
+        getAllStarships(response.data.next);
+      } else {
+        setStarships(newStarships);
+        setLoading(false);
+      }
+    } catch (err) {
+      setError(err.message);
+      setLoading(false);
+    }
+  };
+
+  const getAllPeople = async () => {
+    const results = [];
+    try {
+      for (let i = 1; i < 10; i++) {
+        const response = await axios.get(
+          `https://swapi.dev/api/people/?page=${i}`
+        );
+        results.push = [...response.data.results];
+      }
+    } catch (err) {
+      console.log(err.message);
+    }
+    setPilots(results);
+  };
+
+  useEffect(() => {
+    getAllStarships("https://swapi.dev/api/starships/");
+    getAllPeople();
+  }, []);
+
+  if (error) {
+    return <div>Error: {error}</div>;
+  }
+
+  if (loading) {
+    return <div>Loading...</div>;
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <table>
+        <thead>
+          <tr>
+            <th>Starships</th>
+            <th>Pilots</th>
+          </tr>
+        </thead>
+        <tbody>
+          {starships.map((starship) => (
+            <tr key={starship.name}>
+              <td>{starship.name}</td>
+              <td>
+                {pilots
+                  .filter((pilot) => pilot.starships.includes(starship.url))
+                  .map((pilot) => (
+                    <span key={pilot.name}>{pilot.name}, </span>
+                  ))}
+              </td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
     </div>
   );
-}
+};
 
-export default App;
+export default Starships;
